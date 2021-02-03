6e283663922be35e90ffacedc0dbb88b5fe85edf
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-03 21:26:08 +0530

    add data to cryptoCard_left

--- src/component/CryptoCard.js
+++ src/component/CryptoCard.js
@@ -19,6 +19,21 @@ function CryptoCard({
       <h1>{current_price}</h1>
       <h1>{rank}</h1>
       <h1>{hourChangePercentage.toFixed(2)}</h1> */}
+      <section>
+        <div className="cryptoCard_left">
+          <div className="left_rank">
+            <h1>rank</h1>
+            <p>{rank}</p>
+          </div>
+          <img src={image} alt="" />
+          <div className="left_name">
+            <h1>Name/Symbol</h1>
+            <p>
+              {name} / {symbol}
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
