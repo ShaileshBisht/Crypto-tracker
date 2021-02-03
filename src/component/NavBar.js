9e47aca58b408a9a8cecaa608fe1351b1a15c9a2
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-03 18:11:06 +0530

    add img and title to NavBar left

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,12 +1,18 @@
 import React from "react";
 import "./NavBar.css";
+import coinImg from "../assets/images/navIcon2.png";
 
 function NavBar() {
   return (
     <div className="navBar">
       <section>
         <nav className="navBar_container">
-          <div className="navBar_container-left"></div>
+          <div className="navBar_container-left">
+            <img src={coinImg} alt="" />
+            <h1>
+              crypto<span>Tracker</span>
+            </h1>
+          </div>
           <div className="navBar_container-right"></div>
         </nav>
       </section>
