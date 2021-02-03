e395e59d2a8661435d2fea0bd883bc7157390115
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-03 18:59:57 +0530

    add CryptoCard component and connect it to CryptoCard.css in HomeResult component

---
+++ src/component/CryptoCard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./CryptoCard.css";
+
+function CryptoCard() {
+  return (
+    <div className="cryptoCard">
+      <h1>i am cryptoCard compoent</h1>
+    </div>
+  );
+}
+
+export default CryptoCard;
--- src/component/HomeResult.js
+++ src/component/HomeResult.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./HomeResult.css";
+import CryptoCard from "./CryptoCard";
 
 function HomeResult() {
   return (
     <div className="homeResult">
       <h1>i am home result component</h1>
+      <CryptoCard />
     </div>
   );
 }
