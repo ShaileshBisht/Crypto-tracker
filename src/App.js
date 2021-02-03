599830dcf1dff6ae997b4b520f5831c92d4d9b35
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-03 18:54:03 +0530

    add HomeResult component and connect it to HomeResult.css

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./App.css";
 import NavBar from "./component/NavBar";
+import HomeResult from "./component/HomeResult";
 
 function App() {
   return (
     <div className="app">
       <NavBar />
+      <HomeResult />
     </div>
   );
 }
---
+++ src/component/HomeResult.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HomeResult.css";
+
+function HomeResult() {
+  return (
+    <div className="homeResult">
+      <h1>i am home result component</h1>
+    </div>
+  );
+}
+
+export default HomeResult;
