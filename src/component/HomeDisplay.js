dc43e67504d71ee51eb7fb022bc32142b0870552
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-03 19:36:12 +0530

    set homeData hook to capture the api result

--- src/component/HomeDisplay.js
+++ src/component/HomeDisplay.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./HomeDisplay.css";
 import CryptoCard from "./CryptoCard";
 
 function HomeResult() {
+  const [homeData, setHomeData] = useState("");
+
   return (
     <div className="homeDisplay">
       <h1>i am home display component</h1>
