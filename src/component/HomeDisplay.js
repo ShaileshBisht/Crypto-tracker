0df381a35732dd126b2f6d73fd594ed8e5d7546d
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-03 20:44:25 +0530

    store rest API data to homeData using useEffect

--- src/component/HomeDisplay.js
+++ src/component/HomeDisplay.js
@@ -1,9 +1,23 @@
 import React, { useState, useEffect } from "react";
 import "./HomeDisplay.css";
 import CryptoCard from "./CryptoCard";
+import { createFactory } from "react";
 
 function HomeResult() {
-  const [homeData, setHomeData] = useState("");
+  const [homeData, setHomeData] = useState([]);
+  console.log(homeData);
+
+  useEffect(() => {
+    const coinData = async () => {
+      await fetch(
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=INR&order=market_cap_desc&per_page=100&page=1&sparkline=false&price_change_percentage=1h"
+      )
+        .then((res) => res.json())
+        .then((data) => setHomeData(data));
+    };
+
+    return coinData();
+  }, []);
 
   return (
     <div className="homeDisplay">
