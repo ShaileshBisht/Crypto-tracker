0c3981f527e9ec21266327fda955cc9af9b975b8
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-06 03:26:17 +0530

    add animation to NavBar

--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -2,9 +2,24 @@ import React, { useEffect } from "react";
 import "./NavBar.css";
 import coinImg from "../assets/images/coinnav1.webp";
 import { useHistory } from "react-router-dom";
+import gsap from "gsap";
 
 function NavBar() {
   const history = useHistory();
+
+  useEffect(() => {
+    gsap.from(".coinImg", {
+      duration: 2,
+      rotate: "360deg",
+      repeat: -1,
+      yoyo: true,
+    });
+    gsap.from(".aniTitle", {
+      duration: 2,
+      y: -100,
+      ease: "bounce.out(1, 0.3)",
+    });
+  }, []);
 
   useEffect(() => {
     window.onscroll = () => {
@@ -25,8 +40,8 @@ function NavBar() {
             className="navBar_container-left"
             id="navBar_container-left"
           >
-            <img src={coinImg} alt="" />
-            <h1>
+            <img className="coinImg" src={coinImg} alt="" />
+            <h1 className="aniTitle">
               crypto<strong>Tracker</strong>
             </h1>
           </div>
