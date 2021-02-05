c19a5fe796a6cc3274de3b55ec709a860bc47a67
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-06 00:46:57 +0530

    useing react-Router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from "react";
 import "./App.css";
 import NavBar from "./component/NavBar";
 import HomeDisplay from "./component/HomeDisplay";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="app">
-      <NavBar />
-      <HomeDisplay />
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route exact path="/">
+            <HomeDisplay />
+          </Route>
+          <Route exact path="/coin/detail">
+            <h1>i am detail</h1>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
