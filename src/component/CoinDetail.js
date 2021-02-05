b33bb7b34de9a16c205c6931a60a207d7597f0fe
shaileh Bisht <shaileshbisht765@gmail.com>
2021-02-06 01:20:37 +0530

    get name symbol and categorie

--- src/component/CoinDetail.js
+++ src/component/CoinDetail.js
@@ -20,7 +20,26 @@ function CoinDetail() {
     return coinData();
   }, [coinId]);
 
-  return <div className="coinDetail">i am coin detail</div>;
+  return (
+    <div className="coinDetail">
+      <div className="coinDetail_top">
+        <div className="coinDetail_top-left">
+          <img src={coinData.image.large} alt={coinData.id} />
+        </div>
+        <div className="coinDetail_top-right">
+          <h3>
+            Name: <span>{coinData.name}</span>
+          </h3>
+          <h3>
+            Symbol: <span>{coinData.symbol}</span>
+          </h3>
+          <h3>
+            Categories: <span>{coinData.categories}</span>
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default CoinDetail;
